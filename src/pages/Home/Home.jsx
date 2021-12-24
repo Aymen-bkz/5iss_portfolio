9c7c9106350d4a0b2776b1ee59be69722a9d5ed1
aymenbkzz <aboukezzata@hotmail.fr>
2021-12-24 01:27:17 +0100

    first_page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,11 +5,25 @@ import { Col, Container, Row } from 'react-bootstrap';
 import Work_img from '../../assets/Working.svg'
 import FadeIn from 'react-fade-in'
 import { BsFillArrowDownSquareFill } from 'react-icons/bs'
+import { useSpring, animated } from 'react-spring'
 
 // import Navbar from '../../components/navbar/navbar';
 import './Home.sass'
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
+    const [styles, api] = useSpring(() => ({
+        from: { translateY: 0 },
+    }))
+
+    useEffect(() => {
+        api({
+            translateY: 10,
+            loop: { reverse: true },
+        })
+    }, [])
+
     return (
         <div className='Home'>
             <Container fluid={true}>
@@ -24,9 +38,14 @@ const Home = () => {
                             <img src={Work_img} alt="Working" />
                         </FadeIn>
                     </Col>
-                    <div className='d-flex justify-content-center position-relative text-light-gray' style={{fontSize: "xxx-large"}}>
-                        <FadeIn transitionDuration={300} delay={1800}><BsFillArrowDownSquareFill></BsFillArrowDownSquareFill></FadeIn>
-                        
+                    <div className='d-flex justify-content-center position-relative' style={{ fontSize: "xxx-large" }}>
+                        <FadeIn transitionDuration={300} delay={1800}>
+                            <animated.div style={styles}>
+                                <Link to="/portfolio">
+                                    <BsFillArrowDownSquareFill className='text-light-gray' style={{color: "inherit", textDecoration: "inherit"}}></BsFillArrowDownSquareFill>
+                                </Link>
+                            </animated.div>
+                        </FadeIn>
                     </div>
                 </Row>
 
